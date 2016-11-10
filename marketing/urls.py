c61db7c957deee5c2249a439477d9b5c381b0d5c
Alex Gomez <alex.gomez.1814@gmail.com>
2016-11-09 19:03:06 -0600

    template, views, urls

--- marketing/urls.py
+++ marketing/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 from django.conf import settings
 from django.conf.urls.static import static
-from marketing.views import CrearPromocion
+from marketing.views import CrearPromocion, index 
 
 urlpatterns = [
     url(r'^nueva_promo$', CrearPromocion.as_view(), name='crear_promocion'),
+    url(r'^',index, name='index'),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
